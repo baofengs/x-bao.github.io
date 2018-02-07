c30020d8cda6d6fadeaa94cf6a26586ab9cadbdd
bao <baoyeyene@gmail.com>
2018-02-07 19:01:25 +0800

    Site updated: 2018-02-07 19:01:25

--- js/comment.js
+++ js/comment.js
@@ -1,7 +1,7 @@
 const gitment = new Gitment({
   id: 'sanf.cn', // optional
   owner: 'x-bao',
-  repo: 'x-bao/comments.sanbf.cn.git',
+  repo: 'comments.sanbf.cn.git',
   oauth: {
     client_id: 'cb47ea77291035eb786a',
     client_secret: '1873a59b6cd1c2cba326e7f5161fa903b180276f',
