4082385378b406221b82b7b85b64b947e373932b
bao <baoyeyene@gmail.com>
2018-02-23 19:31:03 +0800

    Site updated: 2018-02-23 19:31:03

--- js/comment.js
+++ js/comment.js
@@ -4,8 +4,10 @@ $(function () {
         owner: 'x-bao',
         repo: 'comments',
         oauth: {
-            client_id: '421d0e999e803d9a5adb',
-            client_secret: '5dac591999594c62d2e850b35fe0bcfa7e8fce04',
+            // client_id: '421d0e999e803d9a5adb',
+            // client_secret: '5dac591999594c62d2e850b35fe0bcfa7e8fce04'
+            client_id: 'cb47ea77291035eb786a',
+            client_secret: '1873a59b6cd1c2cba326e7f5161fa903b180276f'
         }
     });
     gitment.render('comments');
