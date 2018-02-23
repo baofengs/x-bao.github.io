71a84a4805ed760c744e7b5bcab698d889d4ecac
bao <baoyeyene@gmail.com>
2018-02-23 21:33:54 +0800

    Site updated: 2018-02-23 21:33:54

--- js/main.js
+++ js/main.js
@@ -158,7 +158,21 @@ $(function () {
 
 		var $reward = $('.reward-wrapper');
 		$reward.slideToggle();
-	});
+    });
+    
+    var $backToTop = $('#back-to-top');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $backToTop.fadeIn(1000);
+        } else {
+            $backToTop.fadeOut(1000);
+        }
+    });
+
+    $backToTop.click(function () {
+        $('body,html').animate({ scrollTop: 0 });
+    });
 
 	$('body').addClass('queue-in');
 	setTimeout(function() {
--- js/textarea.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('#comments textarea').focus(function() {
-        console.log('textarea is focused...');
-    });
-});
